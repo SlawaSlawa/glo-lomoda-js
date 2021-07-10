c00a394ca058cf2cbba6c3a3163b44b6e6c17739
Slawa <slawapocht4@yandex.ru>
2021-07-10 22:30:27 +0400

    day 4

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 'use strict';
 const headerCityButton = document.querySelector('.header__city-button');
+const cartListGoods = document.querySelector('.cart__list-goods');
+const cartTotalCost = document.querySelector('.cart__total-cost');
+const subheaderСart = document.querySelector('.subheader__cart');
+const cartOverlay = document.querySelector('.cart-overlay');
+
 let hash = location.hash.substring(1);
-
-
 
 const updateLocation = () => {
     const lsLocation = localStorage.getItem('lomoda-location');
@@ -20,6 +23,52 @@ headerCityButton.addEventListener('click', () => {
 });
 
 updateLocation();
+
+const getLocalStorage = () => {
+    return JSON?.parse(localStorage.getItem('cart-lomoda')) || [];
+};
+
+const setLocalStorage = data => {
+    return localStorage.setItem('cart-lomoda', JSON.stringify(data));
+};
+
+const renderCart = () => {
+    cartListGoods.textContent = '';
+    const cartItems = getLocalStorage();
+
+    let totalPrice = 0;
+
+    cartItems.forEach((item, i) => {
+        const tr = document.createElement('tr');
+        tr.innerHTML = `
+            <td>${i + 1}</td>
+            <td>${item.brand} ${item.name}</td>
+            ${item.color ? `<td>${item.color}</td>` : `<td>-</td>`}
+            ${item.size ? `<td>${item.size}</td>` : `<td>-</td>`}
+            <td>${item.cost} &#8381;</td>
+            <td><button class="btn-delete" data-id="${item.id}">&times;</button></td>
+        `;
+        totalPrice += item.cost;
+        cartListGoods.append(tr);
+    });
+
+    cartTotalCost.textContent = totalPrice + ' ₽';
+};
+
+const deleteItemCart = id => {
+    const cartItems = getLocalStorage();
+    const newCartItems = cartItems.filter(item => item.id !== id);
+    setLocalStorage(newCartItems);
+};
+
+cartListGoods.addEventListener('click', evt => {
+    const target = evt.target;
+
+    if (target.matches('.btn-delete')) {
+        deleteItemCart(target.dataset.id);
+        renderCart();
+    }
+});
 
 // blocked scroll
 
@@ -51,12 +100,10 @@ const enableScroll = () => {
 
 // modal window
 
-const subheaderСart = document.querySelector('.subheader__cart');
-const cartOverlay = document.querySelector('.cart-overlay');
-
 const cartModalOpen = () => {
     cartOverlay.classList.add('cart-overlay-open');
     disableScroll();
+    renderCart();
 };
 
 const cartModalClose = () => {
@@ -194,7 +241,9 @@ try {
         return html + `<li class="card-good__select-item" data-id="${i}">${item}</li>`;
     }, '');
 
-    const renderCardGood = ([{ photo, cost, brand, name, sizes, color }]) => {
+    const renderCardGood = ([{ id, photo, cost, brand, name, sizes, color }]) => {
+        const data = { id, brand, name, cost };
+
         cardGoodImage.src = `goods-image/${photo}`;
         cardGoodImage.alt = `${brand} ${name}`;
         cardGoodBrand.textContent = brand;
@@ -214,6 +263,33 @@ try {
         } else {
             cardGoodSizes.style.display = 'none';
         };
+
+        if (getLocalStorage().some(item => item.id === id)) {
+            cardGoodBuy.classList.add('delete');
+            cardGoodBuy.textContent = 'Удалить из корзины';
+        }
+
+        cardGoodBuy.addEventListener('click', () => {
+            if (cardGoodBuy.classList.contains('delete')) {
+                deleteItemCart(id);
+                cardGoodBuy.classList.remove('delete');
+                cardGoodBuy.textContent = 'Добавить в корзину';
+                return;
+            }
+            if (color) {
+                data.color = cardGoodColor.textContent;
+            }
+            if (sizes) {
+                data.size = cardGoodSizes.textContent;
+            }
+
+            cardGoodBuy.classList.add('delete');
+            cardGoodBuy.textContent = 'Удалить из корзины';
+
+            const cardData = getLocalStorage();
+            cardData.push(data);
+            setLocalStorage(cardData);
+        });
     };
 
     cardGoodSelectWrapper.forEach(item => {
